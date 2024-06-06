6ba1b01ecaeef4f4732f7011a5b0d3e004b73fa2
MarkJulian19 <Ktyf351@mail.ru>
2024-06-06 12:21:02 +0300

    tgsaler init

--- tgsaler/__init__.py
+++ tgsaler/__init__.py
@@ -16,3 +16,38 @@ admin_ids = []
 all_orders = {}
 orders_id = 1
 
+
+def orders_send():
+    orders_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    msg = "\n".join(all_orders)
+    if len(all_orders) == 0:
+        return
+    for el in all_orders:
+        # print(el)
+        # print(el[0])
+        orders_markup.add(types.KeyboardButton(str(el[0])))
+    for admin in admin_usres:
+        bot.send_message(admin, msg)
+        bot.send_message(admin, "Какой заказ готов?")
+
+
+def form_bin_mes(usr):
+    ans = "В вашей корзине сейчас:\n"
+    for el in usr["bin"]:
+        if usr["bin"][el] != 1:
+            ans += "x" + str(usr["bin"][el]) + ": "
+        ans += el + "\n"
+    ans += "Сумма:" + str(usr["price"])
+    return ans
+
+
+def form_order(usr, st, id, name):
+    # print(id)
+    ans = str(id) + " заказ:\n"
+    ans += "От пользователя: @" + name + "\n"
+    for el in usr["bin"]:
+        if usr["bin"][el] != 1:
+            ans += "x" + str(usr["bin"][el]) + ": "
+        ans += el + "\n"
+    ans += "На сумму:" + str(usr["price"]) + "\n" + st
+    return ans
