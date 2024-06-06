8d637bbc90f6010c5cef07e57a671c60b521ce87
SannTim <timohaj1@yandex.ru>
2024-06-06 12:02:03 +0300

    added bd_worker

--- tgsaler/bd_worker.py
+++ tgsaler/bd_worker.py
@@ -3,8 +3,29 @@ from . import functionals
 
 
 class db_controller:
-    def __init__(dbinfofile) -> None:
+    def __init__(self, dbinfofile) -> None:
         properties = functionals.ini_to_dict(file_path=dbinfofile)
+        # Параметры подключения к базе данных
+        dbname = properties["dbname"]
+        user = properties["user"]
+        password = properties["password"]
+        host = properties["host"]
+        port = properties["port"]
+        self.conn_string = (
+            f"dbname={dbname} user={user} password={password} host={host} port={port}"
+        )
 
-    def get_all_by_state():
-        pass
+    def get_all_by_category(self, category_id):
+        # Подключение к базе данных
+        conn = sql.connect(self.conn_string)
+        # Создание курсора для выполнения SQL-запросов
+        cursor = conn.cursor()
+
+        select_data_query = f"SELECT * FROM products WHERE category = '{category_id}'"
+        cursor.execute(select_data_query)
+        rows = cursor.fetchall()
+
+        # Закрытие соединения
+        cursor.close()
+        conn.close()
+        return rows
