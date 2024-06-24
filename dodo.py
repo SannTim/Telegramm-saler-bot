629906fd3cbeb7ff572247c36f30cd52cedaaeec
SannTim <timohaj1@yandex.ru>
2024-06-24 03:15:36 +0300

    added command to autocreate html docs

--- dodo.py
+++ dodo.py
@@ -1,3 +1,14 @@
 import glob
 from pathlib import Path
 import os
+
+HTMLINDEX = "docs/_build/html/index.html"
+
+def task_html():
+    """Gen html docs"""
+    return {
+        "actions": ['sphinx-build -M html "./docs/source" "docs/_build"'],
+        "file_dep": [*glob.glob("./docs/source/*"), *glob.glob("./tgsaler/*.py")],
+        "targets": [HTMLINDEX],
+        "clean": True,
+    }
